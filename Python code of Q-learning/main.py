1e4bc18aaa4542c09c33abc197fe1fb89e97dc23
Lin-m-y-my <mengyanglin@link.cuhk.edu.hk>
2025-04-08 16:16:27 +0800

    Upload code for Q-learning

---
+++ Python code of Q-learning/main.py
@@ -0,0 +1,45 @@
+"""
+Replication of Artificial Intelligence, Algorithmic Pricing, and Collusion
+    by: Calvano, Calzolari, Denicolò (2020) in Python
+    at: https://www.aeaweb.org/articles?id=10.1257/aer.20190623
+
+Original code for duopoly model:
+    author: Matteo Courthoud
+    date: 07/05/2021
+    git: https://github.com/matteocourthoud, https://matteocourthoud.github.io/
+
+Code additions and modifications:
+Oligopoly model, code additions and adjustments
+    author: Teele Rossi
+    date: 23/02/2024
+    git: https://github.com/teeross/algo-collusion-with-duopoly-and-oligopoly
+"""
+
+#the following code is made for the duopoly model, please switch the following .py files to an equivalent of an oligopoly model if desired
+from init import model #alternatively change this to init_oligopoly
+from qlearning import simulate_game
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Number of simulations
+num_simulations = 1 #possible to edit the number of simulations as desired
+
+# List to store results of each simulation
+results = []
+
+# Init algorithm and run simulations
+for _ in range(num_simulations):
+    game = model()
+    _, game_instance, _ = simulate_game(game)  # Breaking apart the tuple
+    results.append(game_instance)  # Assuming game_instance contains the Q-values
+
+# Plotting Q-values across simulations. 
+# The following can be modified accordingly to plot exploration rates, and profit dynamics over time.
+for idx, game_instance in enumerate(results):
+    plt.plot(game_instance.Q[0, 0, 0, :], label=f'Simulation {idx + 1}')  # Adjust as needed
+
+plt.legend()
+plt.title("Q-values across Simulations") #example titles, possible to modify
+plt.xlabel("Actions")
+plt.ylabel("Q-value")
+plt.show()
